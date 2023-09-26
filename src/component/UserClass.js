a6f6268e40fd7b9ad86349553282849972e82db7
Sumit Maurya <smithdashdash@gmail.com>
2023-09-26 14:36:01 +0530

    implemented state variable in class and fxnal component

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -4,6 +4,10 @@ class UserClass extends React.Component
 {
     constructor(props){
         super(props);
+        this.state={
+            count :0,
+            count2 : 1,   
+        };
     }
     render(){
         const {name,location,contact} = this.props;
@@ -12,6 +16,7 @@ class UserClass extends React.Component
                 <h1>Name : {name}</h1>
                 <h2> Location : {location} </h2>
                 <h3>Contact : {contact}</h3>
+                <h3>State variable Count value= {this.state.count} and count2 = {this.state.count2} </h3>
             </div>
         )
     }
