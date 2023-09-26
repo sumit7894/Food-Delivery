583360526cbd438a66d1c6bddc772ba8817e7863
Sumit Maurya <smithdashdash@gmail.com>
2023-09-26 16:31:29 +0530

    update state variables in class componenet

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,11 +1,9 @@
 import UserClass from "./UserClass";
-import User from "./User";
 
 const Contact =()=>{
     return(
         <div>
             <h1>Contact Us Page</h1>
-            <User name={"Sumit Maurya function"} location ={"Prayagraj function"} contact={"smithdashdash@gmail.com function"}/>
             <UserClass name={"Sumit Maurya class"} location ={"Prayagraj class"} contact={"smithdashdash@gmail.com class"}/>
         </div>
     );
--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -6,17 +6,23 @@ class UserClass extends React.Component
         super(props);
         this.state={
             count :0,
-            count2 : 1,   
         };
     }
     render(){
         const {name,location,contact} = this.props;
         return(
             <div className="user-card">
+                {/* IN CLASS COMPONENT NEVER UPDATE STATE VARIABLE DIRECTLY   */}
+                <h1>Count : {this.state.count}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        count:this.state.count +1,
+                    });
+                }}>
+                    Count Incrementor</button>
                 <h1>Name : {name}</h1>
                 <h2> Location : {location} </h2>
                 <h3>Contact : {contact}</h3>
-                <h3>State variable Count value= {this.state.count} and count2 = {this.state.count2} </h3>
             </div>
         )
     }
